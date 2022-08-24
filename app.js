2fc6d16a43d68d6e414b7feb27f537e8e52b8c6f
kotobukid <kotobuki@daqua.jp>
2022-08-24 15:11:35 +0900

    expressのhtmlの配信にpugファイルからの読み込みを設定 ただし "/" へのアクセス時にプロジェクトルートに置いたindex/htmlが反応してしまうため、静的ファイル配信の定義を一旦コメントアウト

--- app.js
+++ app.js
@@ -4,21 +4,24 @@ const port = 3000
 
 express.static.mime.define({'application/wasm': ['wasm']})
 
+app.set('view engine', 'pug')
+
 app.use(express.static('./webapp/dist2', {
     setHeaders: (res) => {
         res.set('Cross-Origin-Opener-Policy', 'same-origin');
         res.set('Cross-Origin-Embedder-Policy', 'require-corp');
     }
 }));
-app.use(express.static('.', {
-    setHeaders: (res) => {
-        res.set('Cross-Origin-Opener-Policy', 'same-origin');
-        res.set('Cross-Origin-Embedder-Policy', 'require-corp');
-    }
-}));
+
+// app.use(express.static('.', {
+//     setHeaders: (res) => {
+//         res.set('Cross-Origin-Opener-Policy', 'same-origin');
+//         res.set('Cross-Origin-Embedder-Policy', 'require-corp');
+//     }
+// }));
 
 app.get('/', (req, res) => {
-    res.end(`<html lang="ja"><head><meta charset="utf-8"><title>トップ</title></head><body><div id="container"></div><script src="/index.js" type="module"></script></body></html>`)
+    res.render('index', {});
 });
 
 app.listen(port, () => {
