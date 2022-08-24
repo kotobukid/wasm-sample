eb19b84b3595e3cc7d73b83b79b47ba46add53c5
kotobukid <kotobuki@daqua.jp>
2022-08-24 15:05:36 +0900

    静的ファイル配信元を2つexpressに登録してみる index.jsとwasmtest2.jsの両方をブラウザから取得できるようになった

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ const port = 3000
 express.static.mime.define({'application/wasm': ['wasm']})
 
 app.use(express.static('./webapp/dist2', {
-// app.use(express.static('.', {
     setHeaders: (res) => {
         res.set('Cross-Origin-Opener-Policy', 'same-origin');
         res.set('Cross-Origin-Embedder-Policy', 'require-corp');
     }
-
 }));
+app.use(express.static('.', {
+    setHeaders: (res) => {
+        res.set('Cross-Origin-Opener-Policy', 'same-origin');
+        res.set('Cross-Origin-Embedder-Policy', 'require-corp');
+    }
+}));
+
+app.get('/', (req, res) => {
+    res.end(`<html lang="ja"><head><meta charset="utf-8"><title>トップ</title></head><body><div id="container"></div><script src="/index.js" type="module"></script></body></html>`)
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
