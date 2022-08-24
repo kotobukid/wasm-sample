a5960f51fe4116cda1785df2388e0e1b5022f057
kotobukid <kotobuki@daqua.jp>
2022-08-24 19:28:26 +0900

    ランダムに座標を揺らす部分をRustでやってみようと思ったが現状挫折

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,44 @@
 mod utils;
 
+use getrandom::getrandom;
 use wasm_bindgen::prelude::*;
+// use js_sys::Array;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub struct Point2D {
+    x: i32,
+    y: i32,
+}
+
+#[wasm_bindgen]
+impl Point2D {
+    #[wasm_bindgen(constructor)]
+    pub fn new(x: i32, y: i32) -> Point2D {
+        Point2D { x, y }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn x(&self) -> i32 {
+        self.x
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn y(&self) -> i32 {
+        self.y
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_field(&mut self, x: i32, y: i32) {
+        self.x = x;
+        self.y = y;
+    }
+}
 
 #[wasm_bindgen]
 extern {
@@ -16,4 +48,18 @@ extern {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, wasmtest!");
+}
+
+// #[wasm_bindgen]
+// pub fn shake_points(points: Box<[JsValue]>) -> Box<[Point2D]> {
+//     JsValue::from_serde(points.iter().map(|p| {
+//         return Point2D::new(getrandom(), getrandom());
+//     })).unwrap()
+// }
+
+
+pub fn shake_points(points: Box<[JsValue]>) -> Box<[Point2D]> {
+    JsValue::from_serde(points.iter().map(|p| {
+        return Point2D::new(getrandom(), getrandom());
+    }).collect())
 }
